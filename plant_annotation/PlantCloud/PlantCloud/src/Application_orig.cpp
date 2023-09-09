8d0f6ed0107db91b18dfe4b061a1008d934c6e32
farah <farahsaeedsiddiqi@gmail.com>
2023-09-08 22:35:44 -0400

    update Release version of msi installer

---
+++ plant_annotation/PlantCloud/PlantCloud/src/Application_orig.cpp
@@ -0,0 +1,45 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main_orig(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK) {
+        std::cout << "Error!" << std::endl;
+    }
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
---
+++ plant_annotation/PlantCloud/PlantCloud/src/VertexBufferLayout.h
@@ -0,0 +1,57 @@
+#pragma once
+#include<vector>
+#include "Renderer.h"
+
+struct VertexBufferElement {
+	unsigned int type;
+	unsigned int count;
+	unsigned char normalized;
+
+
+	static unsigned int GetSizeOfType(unsigned int type) {
+		switch (type) 
+		{
+		case GL_FLOAT: return 4;
+		case GL_UNSIGNED_INT: return 4;
+		case GL_UNSIGNED_BYTE: return 1;
+		}
+		ASSERT(false);
+		return 0;
+	}
+};
+
+class VertexBufferLayout 
+{
+private:
+	std::vector<VertexBufferElement> m_Elements;
+	unsigned int m_Stride;
+public:
+	VertexBufferLayout()
+		:m_Stride(0) {}
+
+	template<typename T>
+	void Push(unsigned int count)
+	{
+		//static_assert(false);
+	}
+
+	template<>
+	void Push<float>(unsigned int count)
+	{
+		VertexBufferElement myelem = { GL_FLOAT, count, GL_FALSE };
+		m_Elements.push_back(myelem);
+		m_Stride += count*VertexBufferElement::GetSizeOfType(GL_FLOAT);//sizeof(GLfloat);
+	}
+
+	template<>
+	void Push<unsigned int>(unsigned int count)
+	{
+		VertexBufferElement myelem = { GL_UNSIGNED_INT, count, GL_FALSE };
+		m_Elements.push_back(myelem);
+		m_Stride += count* VertexBufferElement::GetSizeOfType(GL_UNSIGNED_INT); // sizeof(GLuint);
+	}
+	
+	inline const std::vector<VertexBufferElement> GetElements() const { return m_Elements; }
+	inline unsigned int GetStride() const { return m_Stride; }
+
+};
